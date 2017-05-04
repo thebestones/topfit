a446a02d914c168d6427e4f57338d6b85a0bfd0a
A4Alumno <A4Alumno@PCAULA140.aules.local>
2017-05-04 11:31:06 +0200

    cometari hola com estem

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,6 +11,7 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
+    //hola com estem
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
