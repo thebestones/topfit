3983cc7beeb7bede8a2e30552c50faf23e38c19d
Javier <Javier@equipo-PR1.aules.local>
2017-05-04 11:23:57 +0200

    Primer mensaje

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // Hola mundo
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
